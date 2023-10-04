7e1189288c74dc87977407038fc52c25dd77695e
NiqaNabila <117708977+NiqaNabila@users.noreply.github.com>
2023-10-04 20:33:21 +0700

    Add files via upload
    Update modul inverse

--- ADTMat.java
+++ ADTMat.java
@@ -96,7 +96,7 @@ public class ADTMat {
                 System.out.println("Matriks yang anda masukkan adalah :");
                 TulisMATRIKS(M1);
                 System.out.println();
-                
+
                 System.out.println("\nMasukkan Elemen Matriks ke-2: ");
                 for (i = 0; i < n; i++) {
                     for (j = 0; j < n; j++) {
@@ -439,275 +439,117 @@ public class ADTMat {
 	////////// DETERMINAN////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 	public void MenuDeterminan() {
-            MATRIKS M = new MATRIKS();
-            int pil;
-            System.out.println("Anda telah memilih menu Determinan");
-            BacaMATRIKS(M);
 
-            System.out.println("Silahkan pilih metode pencarian determinan [1 = Ekspansi Kofaktor, 2 = Segitiga Atas, 3 = Segitiga Bawah]");
-            System.out.print("Masukkan pilihan: ");
-            pil = sc.nextInt();
-            while (pil != 1 && pil != 2 && pil != 3){
-                    System.out.println("Pilihan salah !! Silahkan pilih metode pencarian determinan [1 = Ekspansi Kofaktor, 2 = Segitiga Atas, 3 = Segitiga Bawah]");
-                    System.out.print("Masukkan pilihan: ");
-                    pil = sc.nextInt();
-                    System.out.println("");
-            }
-            if (pil == 1) {
-                    DeterminanKofaktor(M);
-            } else if (pil == 2){
-                    Segiatas(M);
-            } else if (pil == 3){
-                    Segibawah(M);
-            }
-            MainMenu();
 	}
 
-	public double EkspansiKofaktor(MATRIKS M) {
-            /* Prekondisi: IsPersegi(M) */
-            /* Menghitung nilai determinan M menggunakan ekspansi kofaktor pada baris ke-1 */
-            if (M.NBaris == 1){
-		return M.Data[0][0]; // Jika M adalah matriks 1x1, maka determinannya adalah elemen tunggal di matriks
-            } else {
-		MATRIKS Minor = new MATRIKS(); // Membuat matriks Minor untuk menghitung determinan
-		BuatMATRIKS((M.NBaris - 1), (M.NKolom - 1), Minor); // Inisialisasi matriks Minor
-		
-                int i, j, k, ci, cj;
-		double det;
-                int koef = 1; // Koefisien untuk menghitung determinan
+	// public double EkspansiKofaktor(MATRIKS M) {
 
-		det = 0;
-		if (M.NBaris == 2){ // untuk matriks 2x2, langsung hitung determinannya
-                    det = M.Data[0][0] * M.Data[1][1] - M.Data[0][1] * M.Data[1][0];
-                    return det;
-		} else {
-                    // Jika matriks M lebih besar dari 2x2, lakukan ekspansi kofaktor
-                    for (i = 0; i < M.NBaris; i++){
-			ci = 0; 
-			cj = 0;
-			for (j = 0; j < M.NBaris; j++){
-                            for (k = 0; k < M.NBaris; k++){
-                                if (j != 0 && k != i){
-                                    // Buat minor matriks dengan menghilangkan baris dan kolom yang sesuai
-                                    Minor.Data[ci][cj] = M.Data[j][k];
-                                    cj = cj + 1;
-                                    if (cj > M.NBaris - 2){
-                                        ci = ci + 1;
-                                        cj = 0;
-                                    }
-                                }
-                            }
-                        }
-                        // Hitung determinan dengan mengakumulasi hasil rekursif
-			det = det + koef * ((M.Data[0][i]) * EkspansiKofaktor(Minor));
-			// Ganti tanda koefisien untuk setiap iterasi
-                        koef = -1 * koef;
-                    }
-		}
-            return det;
-            }
-	}
+	// }
 
-	public void DeterminanKofaktor(MATRIKS M) {
-            double det = EkspansiKofaktor(M);
-            MATRIKSKofaktor2(M);
-            System.out.println("Matriks kofaktornya adalah: ");
-            TulisMATRIKS(M);
-            System.out.println("");
-            System.out.print("Determinannya adalah: ");
-            System.out.println(det);
-            M.NDeskripsi = 1;
-            M.Deskripsi[0] = "Matriks Kofaktor, Determinannya adalah " + String.valueOf(det); // Menyimpan deskripsi hasil dalam matriks.
-	}
+	// public void DeterminanKofaktor(MATRIKS M) {
 
-	public double KofakElmt(MATRIKS M, int i, int j) {
-            /* I.S. M terdefinisi, i, j dalam range matriks */
-            /* F.S. Mengeluarkan elemen kofaktor(i,j) dari sebuah matriks */
-            MATRIKS Minor = new MATRIKS();
-            BuatMATRIKS((M.NBaris - 1), (M.NKolom - 1), Minor);
-            int ii, jj, mi, mj;
-            double koef = Math.pow(-1, (i+j+2)) ; // Menghitung koefisien (-1)^(i+j+2) untuk kofaktor
-            mi = 0; 
-            mj = 0;
-            for (ii = 0; ii < M.NBaris; ii++) {
-		for (jj = 0; jj < M.NKolom; jj++) {
-                    if (ii != i && jj != j) {
-			Minor.Data[mi][mj] = M.Data[ii][jj]; // Memasukkan elemen-elemen matriks minor
-			mj = mj + 1;
-			if (mj > M.NBaris - 2) {
-                            mi = mi + 1;
-                            mj = 0;
-			}
-                    }
-		}
-            }
-            return EkspansiKofaktor(Minor)*koef ; // Menghitung kofaktor dengan mengalikan determinan matriks minor dengan koefisien	
-	}
+	// }
 
-	public MATRIKS MATRIKSKofaktor(MATRIKS M) {
-            /* I.S. M terdefinisi */
-            /* F.S. Mengeluarkan Matriks kofaktor dari sebuah matriks */
-            int i,j;
-            MATRIKS Kofaktor = new MATRIKS();
-            BuatMATRIKS(M.NBaris,M.NKolom,Kofaktor);
-            for (i = 0; i < M.NBaris; i++){
-		for (j = 0; j < M.NKolom; j++){
-                    Kofaktor.Data[i][j] = KofakElmt(M, i, j); // Menghitung kofaktor untuk elemen (i, j) dan memasukkannya ke dalam matriks Kofaktor
-		}
-            }
-            return Kofaktor;
-	}
+	// public double KofakElmt(MATRIKS M, int i, int j) {
+
+	// }
+
+	// public MATRIKS MATRIKSKofaktor(MATRIKS M) {
+
+	// }
 
 	public void MATRIKSKofaktor2(MATRIKS M) {
-            /* I.S. M terdefinisi */
-            /* F.S. Mengeluarkan Matriks kofaktor dari sebuah matriks */
-            int i,j;
-            MATRIKS Kofaktor = new MATRIKS();
-            BuatMATRIKS(M.NBaris,M.NKolom,Kofaktor);
-            for (i = 0; i < M.NBaris; i++){
-		for (j = 0; j < M.NKolom; j++){
-                    Kofaktor.Data[i][j] = KofakElmt(M, i, j);
-		}
-            }
-            M.Data = Kofaktor.Data; // Mengganti data matriks input dengan data matriks kofaktor
+
 	}
 
-	public void Segiatas(MATRIKS M) {
-            /* I.S. M terdefinisi, IsPersegi(M) */
-            /* F.S. Menghitung nilai determinan matriks M dengan metode segitiga atas. 
-                Menulis ke layar matriks segitiga atas yang terbentuk dan determinan matriks awal. */
-            int n, i, j; 
-            double line1, line2;
-            float count = 1;
-            boolean found;
-            for (n = 0; n < M.NBaris; n++){
-		if (M.Data[n][n] == 0){
-                    found = false;
-                    i = n+1;
-                    while (!found && i < M.NBaris){
-			if (M.Data[i][n] != 0){
-                            found = true;
-                            for (j = n; j < M.NKolom; j++){
-				M.Data[n][j] += M.Data[i][j];
-                            }
-			}
-			i++;
-                    }
-                    if (!found){
-			count = 0;
-			n = M.NBaris;
-			System.out.println("Matriks tidak dapat membentuk matriks segitiga atas.");
-			System.out.println("Kondisi matriks setelah melakukan OBE:");
-			TulisMATRIKS(M);
-			System.out.println();
-			System.out.println("Determinan matriks = 0.0");
-                    }  
-		}
-		for (i = n+1; i < M.NBaris; i++){       
-                    if (M.Data[i][n] != 0){        
-                        line1 = M.Data[n][n];
-                        line2 = M.Data[i][n]; 
-			for (j = n; j < M.NKolom; j++){
-                            M.Data[n][j] /= line1;
-                            M.Data[i][j] /= line2;
-                            M.Data[i][j] -= M.Data[n][j];
-			}
-			count *= (line1 * line2);
-                    }
-		}
-		count *= M.Data[n][n];
-            }
-            if (count != 0){
-		System.out.println("Matriks segitiga atas berhasil terbentuk.");
-		System.out.println("Matriks segitiga atas tersebut adalah:");
-		TulisMATRIKS(M);
-		System.out.println();
-		System.out.println("Determinan matriks = " + count);
-		M.NDeskripsi = 1;
-		M.Deskripsi[0] = "Matriks Segitiga Atas, Determinannya adalah " + String.valueOf(count);
-            }
-	}
+	// public void Segiatas(MATRIKS M) {
 
-	public void Segibawah(MATRIKS M) {
-            /* I.S. M terdefinisi, IsPersegi(M) */
-            /* F.S. Menghitung nilai determinan matriks M dengan metode segitiga bawah. 
-                Menulis ke layar matriks segitiga bawah yang terbentuk dan determinan matriks awal. */
-            int n, i, j; 
-            double line1, line2;
-            float count = 1;
-            boolean found;
-            for (n = M.NBaris-1; n >= 0; n--){
-		if (M.Data[n][n] == 0){
-                    found = false;
-                    i = n-1;
-                    while (!found && i >= 0){
-			if (M.Data[i][n] != 0){
-                            found = true;
-                            for (j = n; j >= 0; j--){
-                                M.Data[n][j] += M.Data[i][j];
-                            }
-			}
-                        i--;
-                    }
-                    if (!found){
-			count = 0;
-			n = -1;
-			System.out.println("Matriks tidak dapat membentuk matriks segitiga bawah.");
-			System.out.println("Kondisi matriks setelah melakukan OBE:");
-			TulisMATRIKS(M);
-			System.out.println();
-			System.out.println("Matriks Segitiga Bawah, Determinan matriks = 0.0");
-                    }  
-		}
-		for (i = n-1; i >= 0; i--){       
-                    if (M.Data[i][n] != 0){        
-			line1 = M.Data[n][n];
-			line2 = M.Data[i][n]; 
-			for (j = n; j >= 0; j--){
-                            M.Data[n][j] /= line1;
-                            M.Data[i][j] /= line2;
-                            M.Data[i][j] -= M.Data[n][j];
-			}
-			count *= (line1 * line2);
-                    }
-		}
-		count *= M.Data[n][n];
-            }
-            if (count != 0){
-		System.out.println("Matriks segitiga bawah berhasil terbentuk.");
-		System.out.println("Matriks segitiga bawah tersebut adalah:");
-		TulisMATRIKS(M);
-		System.out.println();
-		System.out.println("Determinan matriks = " + count);
-		M.NDeskripsi = 1;
-		M.Deskripsi[0] = "Matriks Segitiga Bawah, Determinannya adalah " + String.valueOf(count);
-            }
-	}
+	// }
+
+	// public void Segibawah(MATRIKS M) {
+
+	// }
 
 	////////// INVERS////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 	public void MenuInvers() {
 
 	}
 
-	// public void InverseKofaktor(MATRIKS M) {
+	public void InverseKofaktor(MATRIKS M) {
+		/* I.S. M terdefinisi */
+		/* F.S. Memprint matriks invers dari matriks M dengan metode determinant dan adjoint */
+		double det = EkspansiKofaktor(M);
+		MATRIKSKofaktor2(M);
+		Transpose(M);
+		System.out.println("Matriks adjoint nya adalah :");
+		TulisMATRIKS(M);
+		System.out.println("");
+		System.out.printf("Determinannya adalah : %.2f", det );
+		System.out.println("");
+		det = 1/det;
+		PKaliKons(M, det);
+		System.out.println("Matriks inverse nya adalah :");
+		TulisMATRIKS(M);	
+		System.out.println();
+	}
 
-	// }
+	public void InverseGaussJordan(MATRIKS M) {
+		System.out.println("Menggabungkan dengan matriks identitas!");
+		System.out.println("Matriksnya adalah :");
+		TulisMATRIKS(MergeInverseJordan(M));
+		System.out.println();
+		System.out.println("Melakukan GaussJordan");
+		TulisMATRIKS(AugmentedInverseJordan(M));
+		System.out.println();
+		System.out.println("Matriksnya  inversnya adalah :");
+		HasilInverseJordan(M);
+		TulisMATRIKS(M);
+		System.out.println();
+	}
 
-	// public void InverseGaussJordan(MATRIKS M) {
+	public MATRIKS AugmentedInverseJordan(MATRIKS M) {
+		// I.S MATRIKS TELAH DI MERGE 
+		// F.S MATRIKS DI OBE
+		MATRIKS M1 = new MATRIKS();
+		M1 = MergeInverseJordan(M);
+		GaussJordan(M1);
+		return M1 ;
+	}
 
-	// }
+	public MATRIKS MergeInverseJordan(MATRIKS M) {
+		// I.S inputan matriks yang mau di inverse 
+		// F.S MERGE MATRIKS
+		int i,j;
+		MATRIKS M1 = new MATRIKS();
+		BuatMATRIKS(M.NBaris, 2*M.NKolom, M1);
+		for (i=0; i <M1.NBaris; i++){
+			for (j=0; j<M1.NKolom; j++){
+				if (j < M.NKolom){
+					M1.Data[i][j] = M.Data[i][j];
+				}else {
+					if ((j- M.NKolom) == i){
+						M1.Data[i][j] = 1.0;
+					}else {
+						M1.Data[i][j] = 0.0;
+					}				
+				}
+			}
+		}
+		return M1;
+	}
 
-	// public MATRIKS AugmentedInverseJordan(MATRIKS M) {
-
-	// }
-
-	// public MATRIKS MergeInverseJordan(MATRIKS M) {
-
-	// }
-
-	// public void HasilInverseJordan(MATRIKS M) {
-
-	// }
+	public void HasilInverseJordan(MATRIKS M) {
+		// I.S MATRIKS TELAH DI OBE KAN  
+		// F.S MATRIKS INVERSE
+		int i,j;
+		MATRIKS M1 = new MATRIKS();
+		M1 = AugmentedInverseJordan(M);
+		for (i=0; i<M.NBaris;i++){
+			for (j=0; j <M.NKolom; j++){
+				M.Data[i][j] = M1.Data[i][j+M.NKolom];
+			}
+		}
+	}
 
 	////////// SPL////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 	public void MenuSPL() {
@@ -751,52 +593,206 @@ public class ADTMat {
 		MainMenu();
 	}
 
-	// public void GetMATRIKSKoefisien(MATRIKS MAug, MATRIKS MK) {
-
-	// }
-
-	// public void GetMATRIKSHasil(MATRIKS MAug, MATRIKS MH) {
-
-	// }
-
 	// public void EliminasiOBE(MATRIKS M, int indeks) {
 
 	// }
 
-	// public void EliminasiOBEjordan(MATRIKS M, int indeks) {
-
-	// }
+	public void EliminasiOBEjordan(MATRIKS M, int indeks) {
+		// I.S INPUTAN MATRIKS dan Indeks yang akan menjadi acuan untuk dikurangi 
+		// F.S semua diatas angka 1 adalah 0 
+		int i,j,a;
+		double kali; 
+		j = 0;
+		while (j < M.NKolom && M.Data[indeks][j] != 1){
+			j+=1;
+		}
+		if (j < M.NKolom){
+			for (i=0; i < indeks; i++){
+				if (M.Data[i][j] != 0){
+					kali = M.Data[i][j];
+					for (a=j; a < M.NKolom; a++){
+						if (j < M.NKolom -1){
+							M.Data[i][a] -= (M.Data[indeks][a] * kali);
+						}
+					}
+				}
+			}
+		}
+	}
 
 	public void GaussSPL(MATRIKS M) {
 
 	}
 
 	public void GaussJordan(MATRIKS M) {
-
+		// I.S MATRIKS M
+		// F.S matriks echelon tereduksi 
+		int i;
+		GaussSPL(M);
+		for (i = 1; i < M.NBaris; i++){
+				EliminasiOBEjordan(M, i);
+		}
 	}
 
 	public void eliminasiBaris(MATRIKS M) {
 
 	}
 
-	// public Boolean isFree(MATRIKS M, int j) {
-
-	// }
-
-	// public boolean isAllZero(MATRIKS M, int i) {
-
-	// }
-
-	public void solusiGauss(MATRIKS M) {
-
+	public Boolean isFree(MATRIKS M, int j) {
+		//  APAKAH MATRIKS dengan indeks  kolom j free
+		int i;
+		boolean isfree;
+		isfree = true;
+		for (i=0; i < M.NBaris; i++ ){
+			if (M.Data[i][j]  != 0){
+				isfree = false;
+			}
+		}
+		return isfree;
 	}
 
-	// public String ParametrikOrUnik(MATRIKS M, int j) {
+	public boolean isAllZero(MATRIKS M, int i) {
+		// apakah pada baris tersebut semuanya bernilai 0
+		int j;
+		boolean isZero = true;
+		for (j =0; j <M.NKolom; j++){
+			if (M.Data[i][j] != 0){
+				isZero = false;
+			}
+		}
+		return isZero;
+	}
 
-	// }
+	public void solusiGauss(MATRIKS M) {
+		// I.S MATRIKS M 
+		// F.S SOLUSI DARI MATRIKS 
+		MATRIKS Gauss = new MATRIKS();
+		CopyMATRIKS(M, Gauss);
+		int i,j,now;
+		boolean firstparam;
+		String[] solusi = new String[100];
+		for (i = 1; i < M.NBaris; i++){
+			EliminasiOBEjordan(M, i);
+		}
+		eliminasiBaris(M);
+		// variabel yang sebenarnya tidak berguna
+		for (j=0; j < M.NKolom - 1; j++){
+			if (isFree(M,j)){
+				solusi[j] = "Free";
+			} else {
+				solusi[j] = ParametrikOrUnik(M,j);
+			}
+		}
+		for (i=0; i < M.NBaris; i++){
+			now = 99;
+			firstparam = true;
+			for (j=0; j <M.NKolom-1; j++){
+				if (M.Data[i][j]!= 0){
+					if (solusi[j] == "Unik" && now != 99){
+						solusi[j] = "Parametrik";
+					}
+					if (solusi[j] == "Unik" && M.Data[i][j]==1){
+						now = j ;
+						if (M.Data[i][M.NKolom-1]==0){
+							solusi[j] ="X"+String.valueOf(j+1)+" :" +" 0" ;
+						} else {
+							solusi[j] ="X"+String.valueOf(j+1)+" : " + String.format("%.2f",M.Data[i][M.NKolom-1]);
+						}
+					}
+					if (solusi[j] == "Parametrik"){
+						if (firstparam && M.Data[i][M.NKolom-1]==0){
+							solusi[now] = solusi[now] +" "+String.valueOf(M.Data[i][j]*-1)+"X"+String.valueOf(j+1);
+							firstparam = false;
+						} else {
+							solusi[now] = solusi[now] +" + "+String.valueOf(M.Data[i][j]*-1)+"X"+String.valueOf(j+1);
+						} 
+					}
+				}
+			}
+		}
+		System.out.println("Solusinya adalah :");
+		for (j=0; j < M.NKolom - 1; j++){
+			if (solusi[j]=="Free" || solusi[j]=="Parametrik"){
+				solusi[j] = "X"+String.valueOf(j+1)+" : " + "Free";
+			}
+			System.out.println(solusi[j]);
+			M.Deskripsi[j] = "\n" + solusi[j];
+		}
+		M.NDeskripsi = M.NKolom - 1;
+		CopyMATRIKS(Gauss, M);
+	}
+
+	public String ParametrikOrUnik(MATRIKS M, int j) {
+		//  APAKAH MATRIKS dengan indeks  kolom parametrik
+		int i, notzero, satu;
+		notzero = 0;
+		satu = 0;
+		for (i=0; i < M.NBaris; i++ ){
+			if (M.Data[i][j]  != 0){
+				notzero = notzero + 1;
+			}
+			if (M.Data[i][j]  == 1){
+				satu = satu + 1;
+			}
+		}
+		if (satu ==1 && notzero ==1){
+			return "Unik";
+		} else {
+			return "Parametrik";
+		}
+	}
 
 	public void solusiGaussJordan(MATRIKS M) {
-
+		// I.S MATRIKS M 
+		// F.S SOLUSI DARI MATRIKS 
+		int i,j,now;
+		boolean firstparam;
+		String[] solusi = new String[100];
+		
+		// variabel yang sebenarnya tidak berguna
+		for (j=0; j < M.NKolom - 1; j++){
+			if (isFree(M,j)){
+				solusi[j] = "Free";
+			} else {
+				solusi[j] = ParametrikOrUnik(M,j);
+			}
+		}
+		for (i=0; i < M.NBaris; i++){
+			now = 99;
+			firstparam = true;
+			for (j=0; j <M.NKolom-1; j++){
+				if (M.Data[i][j]!= 0){
+					if (solusi[j] == "Unik" && now != 99){
+						solusi[j] = "Parametrik";
+					}
+					if (solusi[j] == "Unik" && M.Data[i][j]==1){
+						now = j ;
+						if (M.Data[i][M.NKolom-1]==0){
+							solusi[j] ="X"+String.valueOf(j+1)+" :"+" 0"  ;
+						} else {
+							solusi[j] ="X"+String.valueOf(j+1)+" : " + String.format("%.2f",M.Data[i][M.NKolom-1]);
+						}
+					}
+					if (solusi[j] == "Parametrik"){
+						if (firstparam && M.Data[i][M.NKolom-1]==0){
+							solusi[now] = solusi[now] +" "+String.valueOf(M.Data[i][j]*-1)+"X"+String.valueOf(j+1);
+							firstparam = false;
+						} else {
+							solusi[now] = solusi[now] +" + "+String.valueOf(M.Data[i][j]*-1)+"X"+String.valueOf(j+1);
+						} 
+					}
+				}
+			}
+		}
+		System.out.println("Solusinya adalah :");
+		for (j=0; j < M.NKolom - 1; j++){
+			if (solusi[j]=="Free" || solusi[j]=="Parametrik"){
+				solusi[j] = "X"+String.valueOf(j+1)+" : " + "Free";
+			}
+			System.out.println(solusi[j]);
+			M.Deskripsi[j] = "\n" + solusi[j];
+		}
+		M.NDeskripsi = M.NKolom - 1;
 	}
 
 	public void MainMenu() {
